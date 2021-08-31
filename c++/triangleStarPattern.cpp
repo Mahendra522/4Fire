5e19b6c89b3e7c6ac157f92fe8f19de889cdd315
Mahendra522 <30647136+Mahendra522@users.noreply.github.com>
2021-08-31 14:33:56 +0530

    Create triangleStarPattern.cpp
    This program will create a triangle pattern using star

---
+++ c++/triangleStarPattern.cpp
@@ -0,0 +1,51 @@
+// C++ program for star Pattern.
+// here n = 5, thus total characters in each line is 2n-1, => 9.
+
+// 1111*1111 // 1st Line: * -->1, 1--> 9-1 = 8
+// 111*1*111 // 2nd Line: * -->2, 1--> 9-2 = 7
+// 11*1*1*11 // 3rd Line: * -->3, 1--> 9-3 = 6
+// 1*1*1*1*1 // nth Line: * -->n, 1--> 9-n
+// *1*1*1*1*
+
+#include<iostream>
+#include <cmath>
+
+using namespace std;
+
+void spacePart(int m){
+    for (int j = 1; j <=  m; j++) {
+        cout<<" ";
+    }
+}
+
+void thirdPart(int k){
+    int count = 0;
+    while(count != k){
+        if(count < k){
+            cout<<"*";
+            count++;
+        }
+        if(count<k){
+            cout<<" ";
+            count++;
+        }
+    }
+}
+
+int main(){
+    int n=14;
+    int m = n-1; 
+    int k = 1;
+    for (int i = 1; i <= n && k <= 2*n-1; i++){ 
+        
+        if(m>=0){
+            spacePart(m);
+            thirdPart(k);
+            spacePart(m);    
+        }
+        m--;
+        k+=2;
+        cout<<endl;
+    }    
+    return 0;
+}
